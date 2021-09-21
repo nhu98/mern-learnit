580e732d2996ca5cabc81560b564ca328f759273
nhu98 <huynhquocnhu@gmail.com>
2021-09-21 12:15:16 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "somedeployedUrl";
+    : "https://limitless-castle-43978.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 export const ADD_POST = "ADD_POST";
